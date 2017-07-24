40fbb59374e31f970037485d242d7d73bc5717e7
Serg <serg-gavel@users.noreply.github.com>
2017-07-24 08:43:29 +0300

    upd script.js
    add twitter button function, clear the code

--- scripts/script.js
+++ scripts/script.js
@@ -1,111 +1,33 @@
 var sliderContent = document.getElementsByClassName('content-container');
-// var quoteButton = document.getElementById('quote-btn');
+var quoteText = document.getElementsByClassName('quote');
+var quoteAuthor = document.getElementsByClassName('author');
 var i=1;
-var firstRun=true;
-// function changeContent() {
-//     for(i=0; i<=sliderContent.length; i++)
-//         if(sliderContent.length <=1){
-//             console.log('if suka blia');
-//         }
-//         else{
-//             sliderContent[i].classList.remove('hidden');
-//             console.log('ebatnia');
-//         }
-//
-// }
-function getRandomQuote(){
 
+function getRandomQuote(){
     if(i>=1){
         sliderContent[i-1].classList.add('hidden');
         sliderContent[i-1].classList.remove('visible');
+        sliderContent[i].classList.remove('hidden');
+        sliderContent[i].classList.add('visible');
     }
-    sliderContent[i].classList.remove('hidden');
-    sliderContent[i].classList.add('visible');
-    if(i>=7){
-        // sliderContent[i].classList.add('hidden');
-        // sliderContent[i].classList.remove('visible');
-        // i = math.random(i*i);
-        // sliderContent[i].classList.remove('hidden');
-        // sliderContent[i].classList.add('visible');
+    if(i>=9){
+        i = 0;
+        sliderContent[9].classList.add('hidden');
+        sliderContent[i].classList.remove('hidden');
+        sliderContent[i].classList.add('visible');
     }
     i++;
 }
 function clickedButton(evt){
-    console.log("clickedButton Passed");
-    console.log(evt.target.innerText);
-
     if(evt.target.innerText==="Random Quote"){
-        //random quote function
-        // sliderContent[i].classList.remove('hidden');
-        // sliderContent[i].classList.add('visible');
         getRandomQuote();
     }else if(evt.target.innerText==="tweet"){
-        //tweet function
+        window.open("https://twitter.com/intent/tweet?text="+"Check out my quote machine! \n "+quoteText[i].textContent+" "+'&mdash;'+quoteAuthor[i].textContent+"  \n https://github.com/serg-gavel/RandomQuoteMachine \n ",'_blank');
     }
 }
 function eventLoader(){
-    console.log("eventLoader Passed");
-    if(firstRun===true){
     sliderContent[0].classList.remove('hidden');
     sliderContent[0].classList.add('visible');
-    firstRun=false;
-    }
     document.addEventListener("click",clickedButton);
 }
-
 window.onload = eventLoader();
-
-// var mathButton = document.getElementsByClassName('math-random');
-// mathButton.addEventListener('click', mathRandom);
-//
-//
-// function mathRandom() {
-//     console.log('hj hj hj hj hj h jh jh j');
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var twitterButton = document.getElementsByClassName('tweet-btn');
