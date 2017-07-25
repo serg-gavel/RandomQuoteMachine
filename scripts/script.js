1cda9ed5366cb8e4794494b787e6712b9d8dd3e7
EvilEpicCoder <1st.elixile@gmail.com>
2017-07-25 04:36:47 +0300

    Removed garbage

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,14 @@
 var sliderContent = document.getElementsByClassName('content-container');
-// var quoteButton = document.getElementById('quote-btn');
 var i=0;
 var prevQuote=0;
 var maxQuotes=6;
-var firstRun=true;
-// function changeContent() {
-//     for(i=0; i<=sliderContent.length; i++)
-//         if(sliderContent.length <=1){
-//             console.log('if suka blia');
-//         }
-//         else{
-//             sliderContent[i].classList.remove('hidden');
-//             console.log('ebatnia');
-//         }
-//
-// }
 function getRandomQuote(){
 	prevQuote=i;
 	i=Math.floor(Math.random() * (maxQuotes - 0)) + 0;
+	if(prevQuote==i){
+		getRandomQuote();
+	}
+	
     sliderContent[prevQuote].classList.add('hidden');
     sliderContent[prevQuote].classList.remove('visible');
 
@@ -27,79 +18,14 @@ function getRandomQuote(){
 function clickedButton(evt){
     console.log("clickedButton Passed");
     console.log(evt.target.innerText);
-
     if(evt.target.innerText==="Random Quote"){
         getRandomQuote();
     }else if(evt.target.innerText==="tweet"){
-        //tweet function
-        window.open("https://twitter.com/intent/tweet?text="+"Check out https://github.com/serg-gavel/RandomQuoteMachine \n\" "+sliderContent[i].children[1].children[0].innerText+" -"+sliderContent[1].children[1].children[1].innerText+"\" \n",'_blank');
-        //console.log(sliderContent[1].children[1].children[1].innerText);
+        window.open("https://twitter.com/intent/tweet?text="+"Check out https://github.com/serg-gavel/RandomQuoteMachine \n\" "+sliderContent[i].children[1].children[0].innerText+" -"+sliderContent[1].children[1].children[1].innerText+"\"My first forked project-EEC \n",'_blank');
     }
 }
 function eventLoader(){
-	console.log("eventLoader Passed");
-    if(firstRun===true){
-    sliderContent[0].classList.remove('hidden');
-    sliderContent[0].classList.add('visible');
-    prevQuote=i;
-    firstRun=false;
-    }
+    getRandomQuote();
     document.addEventListener("click",clickedButton);
 }
-
 window.onload = eventLoader();
-
-// var mathButton = document.getElementsByClassName('math-random');
-// mathButton.addEventListener('click', mathRandom);
-//
-//
-// function mathRandom() {
-//     console.log('hj hj hj hj hj h jh jh j');
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var twitterButton = document.getElementsByClassName('tweet-btn');
